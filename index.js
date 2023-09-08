41c065b31d47cdd35953e5ebb85c8a4eb25e12f5
Gabriel Pereira <pcsolucoes.tec@gmail.com>
2023-09-07 23:18:06 -0400

    formulario

---
+++ index.js
@@ -0,0 +1,18 @@
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+email.addEventListener('focus', () => {
+    email.style.borderColor = '#4A5F6A'
+});
+
+email.addEventListener('blur', () => {
+    email.style.borderColor = '#ccc'
+});
+
+password.addEventListener('focus', () => {
+    password.style.borderColor = '#4A5F6A'
+});
+
+password.addEventListener('blur', () => {
+    password.style.borderColor = '#ccc'
+});
